a19ee1774ad2c678d5a3cfca63c4761262f362e4
Jubaer <mdjubaer424@gmail.com>
2025-08-01 23:36:34 +0600

    added signin route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sign-in', function () {
+    return "This is sign in page";
+});
